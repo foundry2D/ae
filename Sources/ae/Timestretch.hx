9e84b15de71646a2fe8b1d0bcfeeaf429d4b9ff0
Unknown <bastiennadeau@gmail.com>
2018-12-04 18:37:37 -0500

    Timestretching and pitch shifting research

---
+++ Sources/ae/Timestretch.hx
@@ -0,0 +1,12 @@
+package ae;
+
+/**
+ * https://smartech.gatech.edu/bitstream/handle/1853/54587/WAC2016-48.pdf
+ * https://smartech.gatech.edu/bitstream/handle/1853/54587/timestretching_videostream.html?sequence=8&isAllowed=y
+ */
+ class Timestrech{
+	public static function stretch(){
+
+ 	}
+
+ }
